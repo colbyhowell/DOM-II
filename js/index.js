3861f52a08bee15112f6409f12fa98cc4f108d36
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-06-07 20:10:32 -0500

    First animation is complete. It's a rotation animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+// 1st item
+
+const logoAnimate = document.querySelector('.logo-heading')
+
+logoAnimate.addEventListener("mouseover", () => {
+    logoAnimate.style.transform = 'rotate(-10deg)'
+})
+
+logoAnimate.addEventListener("mouseleave", () => {
+    logoAnimate.style.transform = 'rotate(0deg)'
+})
+
+console.log(logoAnimate)
