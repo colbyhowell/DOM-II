518c0fd95ec61b98264d46088d62295e48a587b4
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-06-07 22:48:23 -0500

    Managed to get a few more animations working. Still missing 3-4

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ logoAnimate.addEventListener("mouseleave", () => {
 const navMovement = document.querySelector('.main-navigation')
 
 navMovement.addEventListener('scroll', () => {
-    navMovement.style.background = "gray"
+    navMovement.style.backgroundColor = "gray"
 })
 
 
@@ -31,27 +31,57 @@ copyText.addEventListener('copy', () => {
 
 // 4th item
 
-const formButtonChanges = document.querySelector('.adventureForm')
+const formStyling = document.querySelector('.adventureForm')
+formStyling.style.fontSize = "16px"
 
-formButtonChanges.style.fontSize = "16px"
+
+const formButtonChanges = document.getElementById("noRadio")
+
 formButtonChanges.addEventListener('onSelect', () => {
-    if(document.querySelector("input[id=no):checked")){
+    if(formButtonChanges.checked === true){
         window.alert("Quit lying!")
     }
 })
 
 // 5th Item
 
-const pageLoad = document.querySelector('p')
+const pageLoad = document.querySelectorAll('p')
 
-pageLoad.addEventListener('load', () => {
-    pageLoad.addClass('.animateText')
+pageLoad.forEach(item => {
+    item.classList.add('animateText')
+})
+
+pageLoad.forEach(item => {
+    item.addEventListener('load', () => {
+        item.classList.toggle("animateText")
+    })
 })
 
 // 6th Item
 
-const funSunButton = document.querySelectorAll(".btn")
+// const funSunButton = document.querySelectorAll(".btn")
 
-funSunButton.addEventListener('focus' (event) => {
-    funSunButton[0].textContent = "Pick Me!"
+// funSunButton.addEventListener('focus' () => {
+//     funSunButton[0].textContent = "Pick Me!"
+// })
+
+
+// 7th item
+
+const doubleClicks = document.querySelectorAll("img")
+
+doubleClicks.forEach(item => {
+    item.addEventListener('dblclick', () => {
+        window.alert("this is an image")
+    })
+})
+
+// 8th item
+
+const resizeDoc = document.querySelector('.container')
+
+resizeDoc.addEventListener("resize", () => {
+    if(resizeDoc.includes("max-width")){
+        copyText.style.fontSize = "18px"
+    }
 })
